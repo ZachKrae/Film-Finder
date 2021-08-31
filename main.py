c29c599b2ffc6a0668ad5af1e4f2fbbdbc17ce51
unknown <zachakraemer@gmail.com>
2021-08-31 14:47:11 -0400

    altered main.py to use DictReader, so that csv headers will be omitted

--- main.py
+++ main.py
@@ -22,13 +22,17 @@ class StackLayoutExample(StackLayout):
         super().__init__(**kwargs)
 
         with open('film-data.csv', 'r') as csv_file:
-            csv_reader = csv.reader(csv_file)
+            csv_reader = csv.DictReader(csv_file)
             
             for line in csv_reader:
-                a = Label(text=line[0], size_hint=(.5, .5), pos_hint=(0, 0))
-                b = Button(text=line[3], size_hint=(.5, .5), pos_hint=(100, 100))
+                a = Label(text=line["title"], size_hint=(.25, .2), pos_hint=(0, 0))
+                b = Label(text=line["showtime"], size_hint=(.25, .2), pos_hint=(50, 50))
+                c = Label(text=line["location"], size_hint=(.25, .2), pos_hint=(100, 100))
+                d = Button(text="Buy", size_hint=(.25, .2), pos_hint=(150, 150))
                 self.add_widget(a)
                 self.add_widget(b)
+                self.add_widget(c)
+                self.add_widget(d)
 
 class FilmScrapeApp(App):
     pass
